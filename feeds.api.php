1e53c13cd6a1561f706c924c9da51bc6b8822e08
Alex Barth <ab@a.local>
2009-10-12 14:52:06 -0400

    Add new hook hook_feeds_after_import() and api documentation for hooks.

---
+++ feeds.api.php
@@ -0,0 +1,36 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Doxygen API documentation for hooks invoked by Feeds.
+ */
+
+/**
+ * Invoked after a feed source has been imported.
+ *
+ * @param $importer
+ *   FeedsImporter object that has been used for importing the feed.
+ * @param $source
+ *  FeedsSource object that describes the source that has been imported.
+ */
+function hook_feeds_after_import(FeedsImporter $importer, FeedsSource $source) {
+  // See geotaxonomy module's implementation for an example.
+}
+
+/**
+ * Alter mapping targets.
+ *
+ * @param &$targets
+ *   Array containing the targets to be offered to the user. Add to this array
+ *   to expose additional options. Remove from this array to suppress options.
+ *   Remove with caution.
+ * @param $vid
+ *   The vocabulary id
+ */
+function geotaxonomy_feeds_term_processor_targets_alter(&$targets, $vid) {
+  if (variable_get('mymodule_vocabulary_'. $vid, 0)) {
+    $targets['lat'] = t('Latitude');
+    $targets['lon'] = t('Longitude');
+  }
+}
